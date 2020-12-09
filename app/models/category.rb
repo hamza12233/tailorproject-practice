16a565dc82461b145cc0e7d291430bbb28a9cad9
hamza <hafizhamzasaleem4@gmail.com>
2020-12-09 10:24:48 +0500

    association set

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+     validates :name, length: { minimum: 5 }, presence: true
+    has_many :tailor_skill
 end
--- app/models/tailor_skill.rb
+++ app/models/tailor_skill.rb
@@ -1,4 +1,5 @@
 class TailorSkill < ApplicationRecord
     belongs_to :user
     belongs_to :skill
+    belongs_to :categories
 end
